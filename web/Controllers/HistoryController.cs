87576945ce4c12b3e90434120fd83fcae43feab4
micomitja <mitja.sepec99@gmail.com>
2020-11-23 23:58:33 +0100

    History Controller.

---
+++ web/Controllers/HistoryController.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+
+namespace web.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly ResExpertContext _context;
+
+        public HistoryController(ResExpertContext context)
+        {
+            _context = context;
+        }
+
+        // GET: History
+        public async Task<IActionResult> Index()
+        {
+            var resExpertContext = _context.Reservations.Include(r => r.Guest).Include(r => r.Table);
+            return View(await resExpertContext.ToListAsync());
+        }
+
+        // GET: History/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .Include(r => r.Guest)
+                .Include(r => r.Table)
+                .FirstOrDefaultAsync(m => m.ReservationID == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // GET: History/Create
+        public IActionResult Create()
+        {
+            ViewData["GuestID"] = new SelectList(_context.Guests, "GuestID", "Email");
+            ViewData["TableID"] = new SelectList(_context.Tables, "TableID", "TableID");
+            return View();
+        }
+
+        // POST: History/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ReservationID,DateOfReservation,Duration,TableID,GuestID")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reservation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GuestID"] = new SelectList(_context.Guests, "GuestID", "Email", reservation.GuestID);
+            ViewData["TableID"] = new SelectList(_context.Tables, "TableID", "TableID", reservation.TableID);
+            return View(reservation);
+        }
+
+        // GET: History/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            ViewData["GuestID"] = new SelectList(_context.Guests, "GuestID", "Email", reservation.GuestID);
+            ViewData["TableID"] = new SelectList(_context.Tables, "TableID", "TableID", reservation.TableID);
+            return View(reservation);
+        }
+
+        // POST: History/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ReservationID,DateOfReservation,Duration,TableID,GuestID")] Reservation reservation)
+        {
+            if (id != reservation.ReservationID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reservation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservationExists(reservation.ReservationID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GuestID"] = new SelectList(_context.Guests, "GuestID", "Email", reservation.GuestID);
+            ViewData["TableID"] = new SelectList(_context.Tables, "TableID", "TableID", reservation.TableID);
+            return View(reservation);
+        }
+
+        // GET: History/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .Include(r => r.Guest)
+                .Include(r => r.Table)
+                .FirstOrDefaultAsync(m => m.ReservationID == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // POST: History/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return _context.Reservations.Any(e => e.ReservationID == id);
+        }
+    }
+}
