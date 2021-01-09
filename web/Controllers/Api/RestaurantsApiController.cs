74f62122811f49630c9da3d8ba832faa4ca8f9a8
tGitm <timrus99@gmail.com>
2021-01-09 18:33:47 +0100

    Web api-s  created

---
+++ web/Controllers/Api/RestaurantsApiController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+
+namespace web.Controllers_Api
+{
+    [Route("api/v1/Restaurant")]
+    [ApiController]
+    public class RestaurantsApiController : ControllerBase
+    {
+        private readonly ResExpertContext _context;
+
+        public RestaurantsApiController(ResExpertContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RestaurantsApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Restaurant>>> GetRestaurants()
+        {
+            return await _context.Restaurants.ToListAsync();
+        }
+
+        // GET: api/RestaurantsApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Restaurant>> GetRestaurant(int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return restaurant;
+        }
+
+        // PUT: api/RestaurantsApi/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRestaurant(int id, Restaurant restaurant)
+        {
+            if (id != restaurant.RestaurantID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(restaurant).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RestaurantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RestaurantsApi
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Restaurant>> PostRestaurant(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRestaurant", new { id = restaurant.RestaurantID }, restaurant);
+        }
+
+        // DELETE: api/RestaurantsApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Restaurant>> DeleteRestaurant(int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Restaurants.Remove(restaurant);
+            await _context.SaveChangesAsync();
+
+            return restaurant;
+        }
+
+        private bool RestaurantExists(int id)
+        {
+            return _context.Restaurants.Any(e => e.RestaurantID == id);
+        }
+    }
+}
