d94aa68f2bd6d9ae394125acedf0af094d17375d
tGitm <timrus99@gmail.com>
2021-01-10 18:56:41 +0100

    Api auth

--- web/Controllers/Api/RestaurantsApiController.cs
+++ web/Controllers/Api/RestaurantsApiController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using web.Data;
 using web.Models;
+using web.Filters;
 
 namespace web.Controllers_Api
 {
     [Route("api/v1/Restaurant")]
     [ApiController]
+    [ApiKeyAuth]
     public class RestaurantsApiController : ControllerBase
     {
         private readonly ResExpertContext _context;
